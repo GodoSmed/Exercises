0e6f6fb85f41ced87a60e872fc843d33311533ce
GodoSmed <kidscordero@gmail.com>
2026-09-05 22:19:34 -0600

    Completed

--- MatrixMul.java
+++ MatrixMul.java
@@ -1,25 +1,50 @@
+/* 
+ [0][0] = A[0][0] * B[0][0] +  
+          A[0][1] * B[1][0] +
+          A[0][2] * B[2][0]
+ [0][1] = A[0][0] * B[0][1] +
+          A[0][1] * B[1][1] +
+          A[0][2] * B[2][1]
+ [1][0] = A[1][0] * B[0][0] +
+          A[1][1] * B[1][0] +
+          A[1][2] * B[2][0]
+ [1][1] = A[1][0] * B[0][1] +
+          A[1][1] * B[1][1] +
+          A[1][2] * B[2][1]
+*/
+
+import java.util.Scanner;
 public class MatrixMul {
     public static void main(String[] args) {
-        int A[][] = { // [2][3]
-                { 1, 2, 3 },
-                { 4, 5, 6 }
-        };
-        int B[][] = { // [3][2]
-                { 10, 11 },
-                { 20, 21 },
-                { 30, 31 }
-        };
+        Scanner scan = new Scanner(System.in);
+        System.out.print("A Rows: ");
+        int m = scan.nextInt();
+        System.out.print("A Columns: ");
+        int n = scan.nextInt();
+        int[][] A = new int[m][n];
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) 
+                A[i][j] = scan.nextInt();
 
+        System.out.print("B Rows: ");
+        m = scan.nextInt();
+        System.out.print("B Columns: ");
+        n = scan.nextInt();
+        int[][] B = new int[m][n];
+        System.out.println();
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) 
+                B[i][j] = scan.nextInt();
+            
         if (A[0].length != B.length) {
             System.out.println("Imposible");
         } else {
             int[][] C = new int[A.length][B[0].length];
-
-            for (int k = 0; k < C[0].length; k++) { //2
-                for (int i = 0; i < A.length; i++) { //2
-                    for (int j = 0; j < B.length; j++) { //3
-                        C[k][i] = C[k][i] + A[i][j] * B[j][i];
-                        System.err.println(C[k][i]);
+            
+            for (int k = 0; k < A.length; k++) { 
+                for (int i = 0; i < B[0].length; i++) { 
+                    for (int j = 0; j < B.length; j++) { 
+                        C[k][i] += A[k][j] * B[j][i];
                     }
                 }
             }
@@ -30,19 +55,8 @@ public class MatrixMul {
                     System.out.print(" " + C[i][j] + " ");
                 }
                 System.out.print("}\n");
-            }
+            }   
         }
+        scan.close();
     }
 }
-// [0][0] = A[0][0] * B[0][0] +
-//          A[0][1] * B[1][0] +
-//          A[0][2] * B[2][0]
-// [0][1] = A[0][0] * B[0][1] +
-//          A[0][1] * B[1][1] +
-//          A[0][2] * B[2][1]
-// [1][0] = A[1][0] * B[0][0] +
-//          A[1][1] * B[1][0] +
-//          A[1][2] * B[2][0]
-// [1][1] = A[1][0] * B[0][1] +
-//          A[1][1] * B[1][1] +
-//          A[1][2] * B[2][1]
